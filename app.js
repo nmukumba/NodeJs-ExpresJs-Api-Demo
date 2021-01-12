903a4da5d7a48fe7856b0aa2689beda70a233373
Norest Mukumba <norest@trust.net>
2021-01-12 16:54:54 +0200

    add routes and start serve

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
+const morgan = require('morgan');
 
 //utils
 const sequelize = require('./utils/database');
 
+//models
+const User = require('./models/user');
+const Task = require('./models/task');
+
+//routes
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
@@ -16,3 +24,32 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+app.use('/auth', authRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+        .json({
+            error: {
+                message: error.message
+            }
+        })
+});
+
+User.hasMany(Task);
+Task.belongsTo(User);
+
+sequelize.sync()
+    // .sync({
+    //     force: true
+    // })
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
