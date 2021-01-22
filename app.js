8a20cd7ef3b4327a148f4d87ce3df515d56fb431
Norest Mukumba <norest@trust.net>
2021-01-22 17:03:53 +0200

    add cors package and handle errors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const morgan = require('morgan');
 
 //utils
@@ -15,31 +16,25 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+//app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization”)');
+//     next();
+// });
+app.use(cors());
 
-app.use('/auth', authRoutes);
-
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
+app.use('/api/auth', authRoutes);
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-        .json({
-            error: {
-                message: error.message
-            }
-        })
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 User.hasMany(Task);
